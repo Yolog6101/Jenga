0392ad1d0b081a7208524f778ed631cb883e945a
Yuzuki Oe <oe-yuzuki-vs@ynu.jp>
2022-09-05 12:18:13 +0900

    gameover時ジェンガの移動を不可に(+ジェンガ下に土台)

--- Assets/OnCollisionEnterSample.cs
+++ Assets/OnCollisionEnterSample.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+//ある名前空間を持つスクリプトのコンポーネントを使うにはこれ(using そのスクリプトに書かれている名前空間)が必要になる 参考:https://kamikoppu.info/unityengine-consideration/ 参考2:https://creive.me/archives/16164/
+using Microsoft.MixedReality.Toolkit.Input;//NearInteractionGrabbableのある名前空間
+using Microsoft.MixedReality.Toolkit.UI;//ObjectManipulatorのある名前空間
 
 public class OnCollisionEnterSample : MonoBehaviour
 {
@@ -27,8 +30,25 @@ public class OnCollisionEnterSample : MonoBehaviour
         if(collision.gameObject.name=="Plane"){
             //色を黒に変化させる(https://xr-hub.com/archives/3993)
             GetComponent<Renderer>().material.color = Color.black;
-            gameOverText.text="GameOver";
             
+            
+            // //このジェンガを動かせないようにする(https://qiita.com/ryuuuuu000/items/200c4158ca7fc69dc827)
+            // NearInteractionGrabbable movemanage=GetComponent<NearInteractionGrabbable>();
+            // movemanage.enabled=false;
+            // ObjectManipulator manage=GetComponent<ObjectManipulator>();
+            // manage.enabled=false;
+            //ジェンガすべて(つまり、tag="jenga"のオブジェクトすべて)動かせないようにする
+            GameObject[] blocks = GameObject.FindGameObjectsWithTag ("jenga");//"jenga"のタグを持つオブジェクト一覧
+            foreach(var b in blocks){
+                //Debug.Log(b.name);
+                NearInteractionGrabbable othermovemanage=b.GetComponent<NearInteractionGrabbable>();
+                othermovemanage.enabled=false;
+                ObjectManipulator othermanage=b.GetComponent<ObjectManipulator>();
+                othermanage.enabled=false;
+            }
+            
+            //ゲームオーバーの文字表示
+            gameOverText.text="GameOver";
             
         }
     }
