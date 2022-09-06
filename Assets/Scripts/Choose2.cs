ac26bdb723c755dfa8edbf6c81a87fc1d3c56380
xie-ninuo-jc@ynu.jp <xie-ninuo-jc@ynu.jp>
2022-09-06 17:23:45 +0900

    Start画面のボタン変更

---
+++ Assets/Scripts/Choose2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Choose2 : MonoBehaviour
+{
+    Interactable thisButton;
+
+    // Start is called before the first frame update
+
+    void Start () {
+        // this.GetComponent<Button>().onClick.AddListener(OnClick);
+        thisButton = this.GetComponent<Interactable>();
+        thisButton.OnClick.AddListener(OnClick);        
+    }
+
+    void OnClick()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("4");
+    }
+    void Update () {
+
+    }
+}
